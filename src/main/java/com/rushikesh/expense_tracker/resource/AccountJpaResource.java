96d3d3cb655dabeebe00d29704669b2f798c2b24
Rushikesh Pandit <rushikesh.d.pandit@gmail.com>
2023-02-08 12:14:28 +0530

    - Remove @PathVariables and accepting data through RequestBody for Expenses Type

--- src/main/java/com/rushikesh/expense_tracker/resource/AccountJpaResource.java
+++ src/main/java/com/rushikesh/expense_tracker/resource/AccountJpaResource.java
@@ -8,11 +8,11 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rushikesh.expense_tracker.constants.ConstantUtil;
@@ -35,8 +35,8 @@ public class AccountJpaResource {
 	@Autowired
 	private AccountsRepository accountRepository;
 
-	@GetMapping("/users/{userId}/accounts")
-	public ResponseEntity<?> retrieveAccounts(@PathVariable int userId) {
+	@GetMapping("/users/accounts")
+	public ResponseEntity<?> retrieveAccounts(@RequestParam(value = "user") int userId) {
 		ServiceResponse response = new ServiceResponse();
 		Optional<Users> user = userRepository.findById(userId);
 
--- src/main/java/com/rushikesh/expense_tracker/resource/ExpensesTypeJpaResource.java
+++ src/main/java/com/rushikesh/expense_tracker/resource/ExpensesTypeJpaResource.java
@@ -1,20 +1,21 @@
 package com.rushikesh.expense_tracker.resource;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rushikesh.expense_tracker.constants.ConstantUtil;
-import com.rushikesh.expense_tracker.exception.ExpensesTypeNotFoundException;
 import com.rushikesh.expense_tracker.exception.UserNotFoundException;
 import com.rushikesh.expense_tracker.model.ExpensesType;
 import com.rushikesh.expense_tracker.model.Users;
@@ -34,8 +35,8 @@ public class ExpensesTypeJpaResource {
 	@Autowired
 	private ExpensesTypeRepository expensesTypeRepository;
 
-	@GetMapping("/users/{userId}/expensestype")
-	public ResponseEntity<?> retrieveExpensesTypes(@PathVariable int userId) {
+	@GetMapping("/users/expensestype")
+	public ResponseEntity<?> retrieveExpensesTypes(@RequestParam(value = "user") int userId) {
 		ServiceResponse response = new ServiceResponse();
 		Optional<Users> user = userRepository.findById(userId);
 
@@ -51,17 +52,22 @@ public class ExpensesTypeJpaResource {
 				.body(response);
 	}
 
-	@PostMapping("/users/{userId}/expensestype")
-	public ResponseEntity<?> createExpensesType(@PathVariable int userId, @Valid @RequestBody ExpensesType expensesType) {
+	@PostMapping("/users/expensestype")
+	public ResponseEntity<?> createExpensesType(@Valid @RequestBody Users userAccount)  {
 		ServiceResponse response = new ServiceResponse();
-		Optional<Users> user = userRepository.findById(userId);
-
+		Optional<Users> user = userRepository.findById(userAccount.getId().intValue());
 		if(user.isEmpty())
-			throw new UserNotFoundException("id:"+userId);
-		expensesType.setUser(user.get());
-		Collection<ExpensesType> existingExpensesTypes = user.get().getExpensesType();
-		existingExpensesTypes.add(expensesType);
-		expensesTypeRepository.save(expensesType);
+			throw new UserNotFoundException("User with id:" + userAccount.getId() + " not cound");
+
+		List<ExpensesType> existingExpensesTypes = user.get().getExpensesType();
+		List<ExpensesType> expensesTypesToBeAdded = userAccount.getExpensesType();
+
+		expensesTypesToBeAdded.stream()
+		.forEach(expensesType -> {
+			expensesType.setUser(user.get());
+			existingExpensesTypes.add(expensesType);
+			expensesTypeRepository.save(expensesType);
+		});
 
 		response.setStatus(ConstantUtil.RESPONSE_SUCCESS);
 		response.setReturnObject(existingExpensesTypes);
@@ -70,28 +76,29 @@ public class ExpensesTypeJpaResource {
 				.body(response);
 	}
 
-	@PutMapping("/users/{userId}/expensesType")
-	public ResponseEntity<?>  updateExpenseType(@PathVariable int userId, @Valid @RequestBody ExpensesType expensesTypes) {
+	@PutMapping("/users/expensestype")
+	public ResponseEntity<?>  updateExpenseType(@Valid @RequestBody Users userAccount) {
 
 		ServiceResponse response = new ServiceResponse();
-
-		Optional<Users> user = userRepository.findById(userId);
-
+		Optional<Users> user = userRepository.findById(userAccount.getId().intValue());
 		if(user.isEmpty())
-			throw new UserNotFoundException("id:"+userId);
+			throw new UserNotFoundException("User with id:" + userAccount.getId() + " not cound");
 
-		/// Handle Account not found		
-		ExpensesType fetchedExpensesTypes = user.get().getExpensesType().stream()
-				.filter(expensesType -> expensesType.getId().longValue() == expensesTypes.getId().longValue())
-				.findAny()
-				.orElse(null);
-		if(fetchedExpensesTypes == null)
-			throw new ExpensesTypeNotFoundException("id:"+expensesTypes.getId().longValue());
+		List<ExpensesType> existingExpensesTypes = user.get().getExpensesType();
+		List<ExpensesType> expensesTypesToBeUpdated = userAccount.getExpensesType();
+		List<ExpensesType> finalExpensesTypes = new CopyOnWriteArrayList<ExpensesType>();
 
-		fetchedExpensesTypes.setExpenseTypeName(expensesTypes.getExpenseTypeName());
-		expensesTypeRepository.save(fetchedExpensesTypes);
-
-		Collection<ExpensesType> existingExpensesTypes = user.get().getExpensesType();
+		existingExpensesTypes.stream().forEach(existingExpensesType -> {
+			expensesTypesToBeUpdated.stream().forEach(expensType -> {
+				if(existingExpensesType.getId().longValue() == expensType.getId().longValue()) {
+					existingExpensesType.setExpenseTypeName(expensType.getExpenseTypeName());
+				} 
+				finalExpensesTypes.add(existingExpensesType);
+			});
+		});
+		finalExpensesTypes.stream().forEach(finalAccount -> {
+			expensesTypeRepository.save(finalAccount);
+		});
 
 		response.setStatus(ConstantUtil.RESPONSE_SUCCESS);
 		response.setReturnObject(existingExpensesTypes);
